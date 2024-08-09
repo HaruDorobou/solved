1fae4c98cc8daa6c4850c337646c9357ca8b9ab9
HaruDorobou <89964170+HaruDorobou@users.noreply.github.com>
2024-08-09 09:40:33 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int N;
+	cin >> N;
+
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= N - i; j++) {
+			cout << " ";
+		}
+
+		for (int x = 1; x <= i; x++) {
+			cout << "*";
+		}
+		
+		cout << "\n";
+	}
+}
