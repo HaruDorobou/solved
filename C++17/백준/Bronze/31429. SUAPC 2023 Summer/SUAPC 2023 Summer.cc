323d0f0f457855ec8084e51fb0b5caa3c32a3fba
HaruDorobou <89964170+HaruDorobou@users.noreply.github.com>
2024-08-14 17:56:58 +0900

    [Bronze V] Title: SUAPC 2023 Summer, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/31429. SUAPC 2023 Summer/SUAPC 2023 Summer.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int r;
+	cin >> r;
+
+	if (r == 1) { cout << "12 1600"; }
+	if (r == 2) { cout << "11 894"; }
+	if (r == 3) { cout << "11 1327"; }
+	if (r == 4) { cout << "10 1311"; }
+	if (r == 5) { cout << "9 1004"; }
+	if (r == 6) { cout << "9 1178"; }
+	if (r == 7) { cout << "9 1357"; }
+	if (r == 8) { cout << "8 837"; }
+	if (r == 9) { cout << "7 1055"; }
+	if (r == 10) { cout << "6 556"; }
+	if (r == 11) { cout << "6 773"; }
+}
