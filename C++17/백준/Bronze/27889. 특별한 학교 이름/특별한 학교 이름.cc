8d66bdd498b25cb149b7cba24b97af5618940542
HaruDorobou <89964170+HaruDorobou@users.noreply.github.com>
2024-08-28 09:05:31 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+
+    if ( s == "NLCS" ) cout << "North London Collegiate School";
+    if ( s == "BHA" ) cout << "Branksome Hall Asia";
+    if ( s == "KIS" ) cout << "Korea International School";
+    if ( s == "SJA" ) cout << "St. Johnsbury Academy";
+}
