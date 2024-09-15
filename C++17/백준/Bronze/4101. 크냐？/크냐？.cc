30fdde56cb8e566c588f74772af3adfa8576e104
HaruDorobou <89964170+HaruDorobou@users.noreply.github.com>
2024-09-15 19:28:13 +0900

    [Bronze V] Title: 크냐?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/4101. 크냐？/크냐？.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+    while(1)
+    {
+        cin >> a >> b;        
+        if ( a == b && a == 0) { break; }
+        
+        cout << (a > b ? "Yes" : "No") << '\n';
+    }
+}
