cb2862683c151e05cf35923ebb73ffec22a4f9a8
HaruDorobou <89964170+HaruDorobou@users.noreply.github.com>
2024-09-21 12:42:06 +0900

    Update 컵라면 측정하기.cc
    update한다!

--- C++17/백준/Bronze/16479. 컵라면 측정하기/컵라면 측정하기.cc
+++ C++17/백준/Bronze/16479. 컵라면 측정하기/컵라면 측정하기.cc
@@ -1,17 +1,21 @@
 #include <iostream>
 #include <iomanip>
+#include <cmath>
 
 using namespace std;
 
 int main()
 {
-    double k, d1, d2;
-    cin >> k;
-    cin >> d1 >> d2;
+    int k, d1, d2;
+    cin >> k >> d1 >> d2;
 
-    double h = 1;
-
-    h = (k * k) - (((d1 - d2) / 2) * ((d1 - d2) / 2.0)) / h;
-
-    cout << fixed << setprecision(2) << h;
-} 
+    if (d1 == d2)
+    {
+        cout << k * k;
+    }
+    else
+    {
+        float diff = (float)(d1 - d2) / 2;
+        cout << fixed << setprecision(6) << k * k - diff * diff;
+    }
+}
