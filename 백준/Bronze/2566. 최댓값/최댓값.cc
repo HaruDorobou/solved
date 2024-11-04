a58057f260157ce31976556b55d9664d94ab290b
HaruDorobou <89964170+HaruDorobou@users.noreply.github.com>
2024-11-04 13:31:42 +0900

    [Bronze III] Title: 최댓값, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2566. 최댓값/최댓값.cc
+++ 백준/Bronze/2566. 최댓값/최댓값.cc
@@ -1,42 +1,26 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
 
 using namespace std;
 
 int main()
 {
-    vector<pair<int, pair<int, int>>> max_val_line;
-    vector<pair<int, pair<int, int>>> max_cand;
-
-    for (int i = 0; i < 9; i++)
+    int max_val = 0;
+    int max_x = 0, max_y = 0;
+    for(int i = 0; i < 9; i++)
     {
-        for (int j = 0; j < 9; j++)
+        for(int j = 0; j < 9; j++)
         {
-            int number;
-            cin >> number;
+            int num;
+            cin >> num;
 
-            max_val_line.emplace_back(number, make_pair(i, j));
+            if (num > max_val)
+            {
+                max_val = num;
+                max_x = i;
+                max_y = j;
+            }
         }
-
-        auto max_iter = max_element(max_val_line.begin(), max_val_line.end(),
-                                    [](const auto &a, const auto &b)
-                                    { return a.first < b.first; });
-
-        int max_value = max_iter->first;
-
-        max_cand.emplace_back(max_value, max_iter->second);
-
-        max_val_line.clear();
     }
 
-    auto max_iter = max_element(max_cand.begin(), max_cand.end(),
-                                [](const auto &a, const auto &b)
-                                { return a.first < b.first; });
-    
-    auto [x, y] = max_iter->second;
-
-    cout << max_iter->first << '\n' << x + 1 << ' ' << y + 1;
-
-    return 0;
+    cout << max_val << '\n' << max_x + 1 << ' ' << max_y + 1;
 }
