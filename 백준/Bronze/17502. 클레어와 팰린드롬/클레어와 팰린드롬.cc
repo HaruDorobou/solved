51890b3fff8d45818d9244e7da3e4b848806b399
HaruDorobou <89964170+HaruDorobou@users.noreply.github.com>
2024-11-15 11:57:05 +0900

    [Bronze II] Title: 클레어와 팰린드롬, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/17502. 클레어와 팰린드롬/클레어와 팰린드롬.cc
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    string s;
+
+    cin >> n >> s;
+
+    string target = "";
+
+    for (int i = 0; i < s.length(); i++)
+    {
+        target += '?';
+    }
+
+    if (target == s)
+    {
+        s.erase();
+        while (n--)
+        {
+            s += 'a';
+        }
+
+        cout << s;
+        return 0;
+    }
+
+    if (n % 2 == 0)
+    {
+        for (int i = 0; i < (n / 2); i++)
+        {
+            if (s[i] != s[s.length() - 1 - i])
+            {
+                if (s[i] == '?' && s[s.length() - 1 - i] != '?')
+                    s[i] = s[s.length() - 1 - i];
+
+                if (s[i] != '?' && s[s.length() - 1 - i] == '?')
+                    s[s.length() - 1 - i] = s[i];
+            }
+            else if (s[i] == s[s.length() - 1 - i] && s[i] == '?')
+            {
+                s[i] = 'a';
+                s[s.length() - 1 - i] = 'a';
+            }
+        }
+
+        cout << s;
+    }
+    else
+    {
+        if (s[n / 2] == '?')
+            s[n / 2] = 'a';
+
+        for (int i = 0; i < (n / 2); i++)
+        {
+            if (s[i] != s[s.length() - 1 - i])
+            {
+                if (s[i] == '?' && s[s.length() - 1 - i] != '?')
+                    s[i] = s[s.length() - 1 - i];
+
+                if (s[i] != '?' && s[s.length() - 1 - i] == '?')
+                    s[s.length() - 1 - i] = s[i];
+            }
+            else if (s[i] == s[s.length() - 1 - i] && s[i] == '?')
+            {
+                s[i] = 'a';
+                s[s.length() - 1 - i] = 'a';
+            }
+        }
+        cout << s;
+    }
+
+    return 0;
+}
