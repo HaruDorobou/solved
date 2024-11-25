433fb6029cc1fb3e0abf9d3698c5b24c8de00127
HaruDorobou <89964170+HaruDorobou@users.noreply.github.com>
2024-11-25 15:28:16 +0900

    [Silver V] Title: 줄세우기, Time: 32 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/10431. 줄세우기/줄세우기.cc
+++ 백준/Silver/10431. 줄세우기/줄세우기.cc
@@ -1,29 +1,23 @@
 #include <iostream>
-#include <vector>
+#include <memory.h>
 
 using namespace std;
 
 int main()
 {
     int n, tn, step = 0;
-    vector<int> v(20);
+    int arr[20];
     cin >> n;
 
     for (int i = 0; i < n; i++)
     {
         cin >> tn;
-        for (int j = 0; j < 20; j++) cin >> v[j];
-        
+        for (int j = 0; j < 20; j++) cin >> arr[j];
         for (int j = 0; j < 20; j++)
-        {
-            for(int k = j; k < 20; k++)
-            {
-                if (v[j] > v[k]) step += 1;
-            }
-        }
+            for (int k = j; k < 20; k++) if (arr[j] > arr[k]) step += 1;
 
         cout << tn << ' ' << step << '\n';
-        v.assign(v.size(), 0);
+        memset(arr, 0, sizeof(arr));
         step = 0;
     }
     return 0;
