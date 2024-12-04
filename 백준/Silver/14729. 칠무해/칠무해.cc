e670cfdf59c17eb854c467316bab6589d2557499
HaruDorobou <89964170+HaruDorobou@users.noreply.github.com>
2024-12-04 16:54:31 +0900

    [Silver V] Title: 칠무해, Time: 2988 ms, Memory: 76544 KB -BaekjoonHub

---
+++ 백준/Silver/14729. 칠무해/칠무해.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    size_t n;
+    cin >> n;
+    double* arr = new double[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    sort(arr, arr + n);
+
+    for (int i = 0; i < 7; i++)
+    {
+        cout << fixed << setprecision(3) << arr[i] << '\n';
+    }
+
+    return 0;
+}
