0f08da598985ec1d7ade5703c7b45519d7e64b39
HaruDorobou <89964170+HaruDorobou@users.noreply.github.com>
2024-12-06 23:27:37 +0900

    [Bronze II] Title: 너의 이름은 몇 점이니?, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/15813. 너의 이름은 몇 점이니？/너의 이름은 몇 점이니？.cc
+++ 백준/Bronze/15813. 너의 이름은 몇 점이니？/너의 이름은 몇 점이니？.cc
@@ -1,18 +1,5 @@
 #include <iostream>
-#include <map>
-
 using namespace std;
-
-int main()
-{
-    map<char, int> score;
-    for (int i = 0; i < 26; i++) score.emplace('A' + i, i + 1);
-    
-    int leng, sum = 0;
-    string name;
-    cin >> leng >> name;
-
-    for(char c : name)  sum += score[c];
-    cout << sum;
-    return 0;
+int main() {
+    int leng, sum = 0; string name; cin >> leng >> name; for(char c : name) sum += (c - 'A' + 1); cout << sum; return 0;
 }
