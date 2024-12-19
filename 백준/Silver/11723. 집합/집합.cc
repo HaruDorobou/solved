3387d10cb1a3b70fac42b61a2bee04e5ec10e508
HaruDorobou <89964170+HaruDorobou@users.noreply.github.com>
2024-12-19 18:19:55 +0900

    [Silver V] Title: 집합, Time: 544 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.cc
+++ 백준/Silver/11723. 집합/집합.cc
@@ -1,6 +1,8 @@
 #include <iostream>
 using namespace std;
 
+const int ALL_BITS = (1 << 21) - 1;
+
 int main()
 {
     ios_base::sync_with_stdio(0);
@@ -14,20 +16,20 @@ int main()
         string op; cin >> op;
 
         if (op == "all")
-            s = (1 << 21) - 1;
-        else if (op == "empty")
+            s = ALL_BITS;
+        else if (op[0] == 'e')
             s = 0;
         else
         {
             int x;
             cin >> x;
-            if (op == "add")
+            if (op[0] == 'a')
                 s |= (1 << x);
-            else if (op == "remove")
+            else if (op[0] == 'r')
                 s &= ~(1 << x);
-            else if (op == "check")
+            else if (op[0] == 'c')
                 cout << ((s & (1 << x)) ? 1 : 0) << '\n';
-            else if (op == "toggle")
+            else if (op[0] == 't')
                 s ^= (1 << x);
         }
     }
