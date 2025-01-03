6013025c24ee411d6ac66d623e895210a51dd421
HaruDorobou <89964170+HaruDorobou@users.noreply.github.com>
2025-01-03 14:38:19 +0900

    [Silver III] Title: 파도반 수열, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    vector<long long> dp(101);
+
+    dp[0] = 1;
+    dp[1] = 1;
+    dp[2] = 1;
+    dp[3] = 1;
+
+    for(int i = 4; i <= 101; i++)
+        dp[i] = dp[i-2] + dp[i-3];
+
+
+    int tc; cin >> tc;
+    while (tc--)
+    {
+        int c; cin >> c;
+        cout << dp[c] << '\n';
+    }
+}
