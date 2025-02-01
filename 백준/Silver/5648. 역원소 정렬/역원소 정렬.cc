45bf1515d52a93ae2350a265adc8e95bc279464e
HaruDorobou <89964170+HaruDorobou@users.noreply.github.com>
2025-02-01 17:16:55 +0900

    [Silver V] Title: 역원소 정렬, Time: 0 ms, Memory: 2224 KB -BaekjoonHub

---
+++ 백준/Silver/5648. 역원소 정렬/역원소 정렬.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{   
+    ios_base::sync_with_stdio(0);
+    cin.tie(nullptr);
+    long long n; cin >> n;
+    
+    vector<long long> v(n);
+    for(int i = 0; i < n; i++)
+    {
+        cin >> v[i];
+        string num = to_string(v[i]);
+        reverse(num.begin(), num.end());
+        v[i] = stoll(num);
+    }
+
+    sort(v.begin(), v.end());
+    
+    for(long long ll : v)
+        cout << ll << '\n';
+}
