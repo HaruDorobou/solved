5f720e0a4dceee902cb6dad318ad4f11f32420e7
HaruDorobou <89964170+HaruDorobou@users.noreply.github.com>
2025-02-05 13:35:14 +0900

    [Bronze V] Title: Can you add this?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/7891. Can you add this？/Can you add this？.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int tc; cin >> tc;
+    while(tc--)
+    {
+        long long a, b;
+        cin >> a >> b;
+        cout << a + b << '\n';
+    }
+}
